98f261a454c755f36c02901901fc5f195de8f364
Leoci Melo <leocij@hotmail.com>
2019-09-25 22:27:39 -0300

    first commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,62 @@
+const { Router } = require('express')
+const router = Router()
+
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+const verifyToken = require('./verifyToken')
+
+const User = require('../models/User')
+
+router.post('/signup', async (req, res, next) => {
+  const { username, email, password } = req.body
+  const user = new User({
+    username, email, password
+  })
+  user.password = await user.encryptPassword(user.password)
+  await user.save();
+
+  const token = jwt.sign({ id: user._id }, config.secret, {
+    expiresIn: 60 * 60 * 24
+  })
+
+  res.json({ auth: true, token })
+})
+
+router.get('/me', verifyToken, async (req, res, next) => {
+
+  const user = await User.findById(req.userId, { password: 0, __v: 0 })
+  if (!user) {
+    return res.status(404).send('No user found')
+  }
+
+  res.json(user)
+})
+
+router.get('/dashboard', verifyToken, (req, res) => {
+  res.json('dashboard')
+})
+
+router.post('/signin', async (req, res, next) => {
+
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    return res.status(404).send('The email doesn\'t exists')
+  }
+
+  const validPassword = await user.validatePassword(password)
+
+  if (!validPassword) {
+    return res.status(401).json({auth: false, token: null})
+  }
+
+  const token = jwt.sign({ id: user._id }, config.secret, {
+    expiresIn: 60 * 60 * 24
+  })
+
+  res.json({ auth: true, token })
+})
+
+module.exports = router
